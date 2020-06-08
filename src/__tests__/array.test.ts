cf97d295276f5511b18d90729495b0eb7988fc2b
Basarat <basaratali@gmail.com>
2020-06-09 08:50:45 +1000

    better

--- src/__tests__/array.test.ts
+++ src/__tests__/array.test.ts
@@ -1,12 +1,51 @@
-import { isArray, isEqual } from '../array';
+import {
+  isArray,
+  isEqual,
+  max,
+  min,
+  sum,
+  average,
+  concat,
+  unique,
+  range
+} from '../array';
 
 test('isArray', () => {
-  expect(isArray([])).toBe(true);
-  expect(isArray('')).toBe(false);
+  expect(isArray([])).toStrictEqual(true);
+  expect(isArray('')).toStrictEqual(false);
 });
 
 test('isEqual', () => {
-  expect(isEqual([1, 2, 3], [1, 2, 3])).toBe(true);
-  expect(isEqual([1, 2, 3], [1, 2])).toBe(false);
-  expect(isEqual([1, 2, 3], [1, '2', 3])).toBe(false);
+  expect(isEqual([1, 2, 3], [1, 2, 3])).toStrictEqual(true);
+  expect(isEqual([1, 2, 3], [1, 2])).toStrictEqual(false);
+  expect(isEqual([1, 2, 3], [1, '2', 3])).toStrictEqual(false);
+});
+
+test('max', () => {
+  expect(max([1, 2, 3])).toStrictEqual(3);
+});
+
+test('min', () => {
+  expect(min([1, 2, 3])).toStrictEqual(1);
+});
+
+test('sum', () => {
+  expect(sum([1, 2, 3])).toStrictEqual(6);
+});
+
+test('average', () => {
+  expect(average([1, 2, 3])).toStrictEqual(2);
+  expect(average([])).toStrictEqual(NaN);
+});
+
+test('concat', () => {
+  expect(concat([1, 2], [3, 4])).toStrictEqual([1, 2, 3, 4]);
+});
+
+test('unique', () => {
+  expect(unique([1, 2, 2, 3])).toStrictEqual([1, 2, 3]);
+});
+
+test('range', () => {
+  expect(range(3, 7)).toStrictEqual([3, 4, 5, 6, 7]);
 });
