badacb84b8bf3d7c90b80e44c05b07877ee6c359
Basarat <basaratali@gmail.com>
2020-06-09 08:30:06 +1000

    cleaner

--- src/array.ts
+++ src/array.ts
@@ -1,31 +1,31 @@
-export function isArray(array: unknown): array is Array<unknown> {
+export function isArray(array: unknown): array is unknown[] {
   return Array.isArray(array);
 }
 
-export function isEqual<T>(a: Array<T>, b: Array<T>) {
+export function isEqual<T>(a: T[], b: T[]) {
   return a.length == b.length && a.every((v, i) => v === b[i]);
 }
 
-export function max(array: Array<number>): number {
+export function max(array: number[]): number {
   return Math.max(...array);
 }
 
-export function min(array: Array<number>): number {
+export function min(array: number[]): number {
   return Math.min(...array);
 }
 
-export function sum(array: Array<number>): number {
+export function sum(array: number[]): number {
   return array.reduce((acc, v) => acc + v, 0);
 }
 
-export function average(array: Array<number>): number {
+export function average(array: number[]): number {
   return sum(array) / array.length;
 }
 
-export function concat<T>(a: Array<T>, b: Array<T>): Array<T> {
+export function concat<T>(a: T[], b: T[]): T[] {
   return a.concat(b);
 }
 
-export function unique<T>(array: Array<T>): Array<T> {
+export function unique<T>(array: T[]): T[] {
   return Array.from(new Set(array));
 }
