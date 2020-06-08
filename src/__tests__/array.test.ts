5115ef2b55c5b6f1d4eba1a461c01c38708b3946
Basarat <basaratali@gmail.com>
2020-06-09 08:40:57 +1000

    wip

--- src/__tests__/array.test.ts
+++ src/__tests__/array.test.ts
@@ -1,6 +1,12 @@
-import { isArray } from '../array';
+import { isArray, isEqual } from '../array';
 
 test('isArray', () => {
   expect(isArray([])).toBe(true);
   expect(isArray('')).toBe(false);
+});
+
+test('isEqual', () => {
+  expect(isEqual([1, 2, 3], [1, 2, 3])).toBe(true);
+  expect(isEqual([1, 2, 3], [1, 2])).toBe(false);
+  expect(isEqual([1, 2, 3], [1, '2', 3])).toBe(false);
 });
