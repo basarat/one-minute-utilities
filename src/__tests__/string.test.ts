2ee1065e65a127e2806dae280e33d20ff4c929b6
Basarat <basaratali@gmail.com>
2020-06-11 08:37:26 +1000

    next

--- src/__tests__/string.test.ts
+++ src/__tests__/string.test.ts
@@ -1,5 +1,10 @@
-import { reverse } from "../string";
+import { reverse, isPalindrome } from "../string";
 
 test('reverse', () => {
   expect(reverse('hello world')).toStrictEqual('dlrow olleh');
+});
+
+test('isPalindrome', () => {
+  expect(isPalindrome('abc')).toStrictEqual(false);
+  expect(isPalindrome('abcba')).toStrictEqual(true);
 });
--- src/string.ts
+++ src/string.ts
@@ -1,3 +1,7 @@
 export function reverse(str: string): string {
   return str.split('').reverse().join('');
 }
+
+export function isPalindrome(str: string): boolean {
+  return str === str.split('').reverse().join('');
+}
