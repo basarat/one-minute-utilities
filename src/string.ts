f2208af1d6487b4539aefc4260323e6861f54707
Basarat Ali Syed <basaratali@gmail.com>
2020-06-12 12:03:13 +1000

    Update string.ts

--- src/string.ts
+++ src/string.ts
@@ -18,13 +18,6 @@ export function upperCaseFirst(str: string): string {
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 }
 
-export function upperCaseWords(str: string): string {
-  return str
-    .split(' ')
-    .map(w => `${w.charAt(0).toUpperCase()}${w.slice(1)}`)
-    .join(' ');
-}
-
 export function splitLines(str: string): string[] {
   return str.split(/\r?\n/);
 }
