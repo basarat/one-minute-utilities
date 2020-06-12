bf7d4ddf4b4f47290127ccae5df3f4d5fa43f1de
Basarat Ali Syed <basaratali@gmail.com>
2020-06-12 11:50:07 +1000

    Update string.ts

--- src/string.ts
+++ src/string.ts
@@ -10,12 +10,12 @@ export function areAnagrams(a: string, b: string): boolean {
   return a.split('').sort().join('') === b.split('').sort().join('');
 }
 
-export function upperCaseFirst(str: string): string {
-  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
-}
-
 export function lowerCaseFirst(str: string): string {
   return `${str.charAt(0).toLowerCase()}${str.slice(1)}`;
+}
+
+export function upperCaseFirst(str: string): string {
+  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 }
 
 export function upperCaseWords(str: string): string {
